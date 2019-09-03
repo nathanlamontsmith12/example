9d83b73988b809b5de8a7728050c723ceb73ce00
Admin <admin@Admins-MacBook-Pro.local>
2019-09-03 15:45:44 -0500

    set up example

--- src/App.js
+++ src/App.js
@@ -2,25 +2,40 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+	constructor(){
+		super()
+		this.state = {
+			name: null
+		}
+	}
+	async componentDidMount(){
+		const example = await fetch('http://localhost:9000/demo-postman', {
+			method: 'POST',
+			credentials: 'include',
+			body: JSON.stringify({'name': 'Nate Smith'}),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		})
+
+		// console.log(example)
+
+		const parsedResponse = await example.json()
+
+		// console.log(parsedResponse)
+		this.setState({
+			name: parsedResponse.dataRecieved.name
+		})
+	}
+	render(){
+		console.log(this.state)
+		return (
+			<div className="App">
+				{this.state.name ? <h1> {this.state.name} </h1> : null}
+			</div>
+		);
+	}
 }
 
 export default App;
